e8f1435026004a03e5475682bada832b27b880c2
David <singerous@gmail.com>
2019-05-29 09:50:29 +0700

    update cnn
    * add validate file support
    * add researches

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -26,19 +26,29 @@ def add_output_layer(model):
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
 
-def load_data(datadir, className):
+def load_data(datadir, className, isTest):
     pickles = []
-    pickleIn = open(datadir + "\\" + className + "\\" + "pickles\\" + "x" + className + ".pickle", "rb")
+    if (isTest):
+        pickleIn = open(datadir + "\\" + className + "\\" + "pickles\\" + "x" + className + "Validate" + ".pickle", "rb")
+    else:
+        pickleIn = open(datadir + "\\" + className + "\\" + "pickles\\" + "x" + className + ".pickle", "rb")
     pickles.append(pickle.load(pickleIn))
-    pickleIn = open(datadir + "\\" + className + "\\" + "pickles\\" + "y" + className + ".pickle", "rb")
+    if (isTest):
+        pickleIn = open(datadir + "\\" + className + "\\" + "pickles\\" + "y" + className + "Validate" + ".pickle", "rb")
+    else:
+        pickleIn = open(datadir + "\\" + className + "\\" + "pickles\\" + "y" + className + ".pickle", "rb")
     pickles.append(pickle.load(pickleIn))
     return pickles
 
-def save_model(model, datadir, className):
+def save_model(model, datadir, className, CNNArchitecture, CNNparameters):
     modelJson = model.to_json()
-    with open(datadir + "\\" + className + "\\models\\" + className + ".json", 'w') as jsonFile:
+    with open(datadir + "\\" + className + "\\models\\" + className + "_conv[" + str(CNNArchitecture["numOfConvLayers"])
+              + "]_" + "filters[" + str(CNNArchitecture["filters"]) + "]_" + "batches[" + str(CNNparameters["batchSize"])
+              + "]" + ".json", 'w') as jsonFile:
         jsonFile.write(modelJson)
-    model.save_weights(datadir + "\\" + className + "\\models\\" + className + ".h5")
+    model.save_weights(datadir + "\\" + className + "\\models\\" + className + "_conv["
+                       + str(CNNArchitecture["numOfConvLayers"]) + "]_" + "filters[" + str(CNNArchitecture["filters"])
+                       + "]_" + "batches[" + str(CNNparameters["batchSize"]) + "]" + ".h5")
 
 def load_model(datadir, modelName):
     jsonFile = open(datadir + "\\models\\" + modelName + ".json", 'r')
@@ -79,8 +89,8 @@ def train_model(model, CNNparameters, trainData, validateData, board):
 #cnn architectures:
 ########################################################################################################################
 genderCNNArchitecture = {
-    'numOfConvLayers': 3,
-    'filters': 64,
+    'numOfConvLayers': 2,
+    'filters': 32,
     'kernelSize': 3,
     'numOfDenseLayers': 0,
     'units': 0,
@@ -88,8 +98,8 @@ genderCNNArchitecture = {
 }
 
 ageCNNArchitecture = {
-    'numOfConvLayers': 3,
-    'filters': 64,
+    'numOfConvLayers': 2,
+    'filters': 32,
     'kernelSize': 3,
     'numOfDenseLayers': 0,
     'units': 0,
@@ -97,8 +107,8 @@ ageCNNArchitecture = {
 }
 
 hairColorCNNArchitecture = {
-    'numOfConvLayers': 3,
-    'filters': 64,
+    'numOfConvLayers': 2,
+    'filters': 32,
     'kernelSize': 3,
     'numOfDenseLayers': 0,
     'units': 0,
@@ -109,18 +119,18 @@ hairColorCNNArchitecture = {
 #cnn parameters
 ########################################################################################################################
 genderCNNparameters = {
-    'batchSize': 8,
-    'numOfEpochs': 3
+    'batchSize': 16,
+    'numOfEpochs': 5
 }
 
 ageCNNparameters = {
-    'batchSize': 8,
-    'numOfEpochs': 3
+    'batchSize': 16,
+    'numOfEpochs': 5
 }
 
 hairColorCNNparameters = {
-    'batchSize': 8,
-    'numOfEpochs': 3
+    'batchSize': 16,
+    'numOfEpochs': 5
 }
 
 genderClassName = "gender"
@@ -128,43 +138,114 @@ ageClassName = "age"
 hairColorClassName = "hair_color"
 
 dir = "C:\\Users\\singe\\Documents\\Human Classifier"
-genderTrain = load_data(dir, genderClassName)
-ageTrain = load_data(dir, ageClassName)
-hairColorTrain = load_data(dir, hairColorClassName)
+genderTrain = load_data(dir, genderClassName, False)
+ageTrain = load_data(dir, ageClassName, False)
+hairColorTrain = load_data(dir, hairColorClassName, False)
+genderTrainValidate = load_data(dir, genderClassName, True)
+ageTrainValidate = load_data(dir, ageClassName, True)
+hairColorTrainValidate = load_data(dir, hairColorClassName, True)
 ########################################################################################################################
 
 #process logs
 ########################################################################################################################
-genderLogName = "{}-conv-{}-filters-{}-batches-{}".format(genderCNNArchitecture["numOfConvLayers"],
+genderLogName = "gender-{}-conv-{}-filters-{}-batches-{}".format(genderCNNArchitecture["numOfConvLayers"],
                                                     genderCNNArchitecture["filters"],
                                                     genderCNNparameters["batchSize"],
                                                     int(time.time()))
 genderBoard = TensorBoard(log_dir='C:\\Users\\singe\\Documents\\Human Classifier\\gender\\logs\\{}'.format(genderLogName))
 
-ageLogName = "{}-conv-{}-filters-{}-batches-{}".format(ageCNNArchitecture["numOfConvLayers"],
+ageLogName = "age-{}-conv-{}-filters-{}-batches-{}".format(ageCNNArchitecture["numOfConvLayers"],
                                                     ageCNNArchitecture["filters"],
                                                     ageCNNparameters["batchSize"],
                                                     int(time.time()))
 ageBoard = TensorBoard(log_dir='C:\\Users\\singe\\Documents\\Human Classifier\\age\\logs\\{}'.format(ageLogName))
 
-hairColorLogName = "{}-conv-{}-filters-{}-batches-{}".format(hairColorCNNArchitecture["numOfConvLayers"],
+hairColorLogName = "hair_color-{}-conv-{}-filters-{}-batches-{}".format(hairColorCNNArchitecture["numOfConvLayers"],
                                                     hairColorCNNArchitecture["filters"],
                                                     hairColorCNNparameters["batchSize"],
                                                     int(time.time()))
 hairColorBoard = TensorBoard(log_dir='C:\\Users\\singe\\Documents\\Human Classifier\\hair_color\\logs\\{}'.format(hairColorLogName))
-########################################################################################################################
+#######################################################################################################################
 
 #build & train models
 ########################################################################################################################
 genderModel = build_model(genderCNNArchitecture, genderTrain)
-train_model(genderModel, genderCNNparameters, genderTrain, genderTrain, genderBoard)
-save_model(genderModel, dir, genderClassName)
+train_model(genderModel, genderCNNparameters, genderTrain, genderTrainValidate, genderBoard)
+save_model(genderModel, dir, genderClassName, genderCNNArchitecture, genderCNNparameters)
 
 ageModel = build_model(ageCNNArchitecture, ageTrain)
-train_model(ageModel, ageCNNparameters, ageTrain, ageTrain, ageBoard)
-save_model(ageModel, dir, ageClassName)
+train_model(ageModel, ageCNNparameters, ageTrain, ageTrainValidate, ageBoard)
+save_model(ageModel, dir, ageClassName, ageCNNArchitecture, ageCNNparameters)
 
 hairColorModel = build_model(hairColorCNNArchitecture, hairColorTrain)
-train_model(hairColorModel, hairColorCNNparameters, hairColorTrain, hairColorTrain, hairColorBoard)
-save_model(hairColorModel, dir, hairColorClassName)
+train_model(hairColorModel, hairColorCNNparameters, hairColorTrain, hairColorTrainValidate, hairColorBoard)
+save_model(hairColorModel, dir, hairColorClassName, hairColorCNNArchitecture, hairColorCNNparameters)
+########################################################################################################################
+
+#research architecture & parameters of cnn
+########################################################################################################################
+# researchNumOfConvLayers = [1, 2, 3]
+# researchFilters = [16, 32]
+# researchBatchSize = [8, 16, 32]
+#
+# researchCNNArchitecture = {
+#     'numOfConvLayers': 2,
+#     'filters': 16,
+#     'kernelSize': 3,
+#     'numOfDenseLayers': 0,
+#     'units': 0,
+#     'poolSize': 2
+# }
+#
+# researchCNNparameters = {
+#     'batchSize': 16,
+#     'numOfEpochs': 8
+# }
+########################################################################################################################
+
+#research code
+########################################################################################################################
+# for numOfConvLayers in researchNumOfConvLayers:
+#     for filters in researchFilters:
+#         for batchSize in researchBatchSize:
+#             researchCNNArchitecture.pop("numOfConvLayers")
+#             researchCNNArchitecture.pop("filters")
+#             researchCNNparameters.pop("batchSize")
+#             researchCNNArchitecture.setdefault("numOfConvLayers", numOfConvLayers)
+#             researchCNNArchitecture.setdefault("filters", filters)
+#             researchCNNparameters.setdefault("batchSize", batchSize)
+#
+#             genderLogName = "gender-{}-conv-{}-filters-{}-batches-{}".format(researchCNNArchitecture["numOfConvLayers"],
+#                                                                              researchCNNArchitecture["filters"],
+#                                                                              researchCNNparameters["batchSize"],
+#                                                                              int(time.time()))
+#             genderBoard = TensorBoard(
+#                 log_dir='C:\\Users\\singe\\Documents\\Human Classifier\\gender\\logs\\{}'.format(genderLogName))
+#
+#             ageLogName = "age-{}-conv-{}-filters-{}-batches-{}".format(researchCNNArchitecture["numOfConvLayers"],
+#                                                                        researchCNNArchitecture["filters"],
+#                                                                        researchCNNparameters["batchSize"],
+#                                                                        int(time.time()))
+#             ageBoard = TensorBoard(
+#                 log_dir='C:\\Users\\singe\\Documents\\Human Classifier\\age\\logs\\{}'.format(ageLogName))
+#
+#             hairColorLogName = "hair_color-{}-conv-{}-filters-{}-batches-{}".format(
+#                 researchCNNArchitecture["numOfConvLayers"],
+#                 researchCNNArchitecture["filters"],
+#                 researchCNNparameters["batchSize"],
+#                 int(time.time()))
+#             hairColorBoard = TensorBoard(
+#                 log_dir='C:\\Users\\singe\\Documents\\Human Classifier\\hair_color\\logs\\{}'.format(hairColorLogName))
+#
+#             genderModel = build_model(researchCNNArchitecture, genderTrain)
+#             train_model(genderModel, researchCNNparameters, genderTrain, genderTrainValidate, genderBoard)
+#             save_model(genderModel, dir, genderClassName, researchCNNArchitecture, researchCNNparameters)
+#
+#             ageModel = build_model(researchCNNArchitecture, ageTrain)
+#             train_model(ageModel, researchCNNparameters, ageTrain, ageTrainValidate, ageBoard)
+#             save_model(ageModel, dir, ageClassName, researchCNNArchitecture, researchCNNparameters)
+#
+#             hairColorModel = build_model(researchCNNArchitecture, hairColorTrain)
+#             train_model(hairColorModel, researchCNNparameters, hairColorTrain, hairColorTrainValidate, hairColorBoard)
+#             save_model(hairColorModel, dir, hairColorClassName, researchCNNArchitecture, researchCNNparameters)
 ########################################################################################################################
